899b1a48a6604051870614fffcc78c19fd1590af
jeromyyannai <jeromyyannai@gmx.com>
2020-06-24 04:01:10 +0200

    update viewoplayer

--- app/src/main/java/com/az/pplayer/PhpPlayerApp.java
+++ app/src/main/java/com/az/pplayer/PhpPlayerApp.java
@@ -3,12 +3,15 @@ package com.az.pplayer;
 import android.app.Application;
 import android.content.Context;
 
+import com.az.pplayer.Services.DownloadService;
+
 public class PhpPlayerApp extends Application {
     private static Context context;
 
     public void onCreate() {
         super.onCreate();
         PhpPlayerApp.context = getApplicationContext();
+        DownloadService.Get();
     }
 
     public static Context getAppContext() {
